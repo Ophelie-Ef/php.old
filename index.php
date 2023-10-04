71fff38eaab914a0f4fa4e551c3a246a1cc6209c
Your Name <you@example.com>
2023-10-04 10:27:04 +0200

    Initialisation PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include('./header.php');
+
+
+echo "<h1>Cours PHP</h1>";
+?>
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis eveniet doloribus, aliquam amet ex non aperiam corrupti voluptatibus hic nisi cumque officia minus odit impedit esse ipsam quas praesentium rem?</p>
+
+<?php
+echo "Encore le cours PHP";
+
+include('./footer.php');
+?>
