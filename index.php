6bae2bd355eec46bd6bf6344f217436aedf03436
Your Name <you@example.com>
2023-10-04 16:38:57 +0200

    Initialisation branche Tableaux

--- index.php
+++ index.php
@@ -4,61 +4,6 @@
 
 require './fonctions.php';
 
-$titre = 'Opérateurs PHP';
-
-/* 
-
-Opérateurs :
-+ //addition
-- //soustraction
-* //multiplication
-/ //division
-** //exposant (aka puissance)
-% //modulo (reste de la division)
-
-*/
-
-// $calcul = $x + $z * $y;
-
-
-$x = 4;
-
-$y = 2;
-
-$z = 5;
-
-
-
-//php respecte les règles des priorités de calculs en maths. Si on veut imposer un ordre de calcul, comme en maths, on ajoute des parenthèses
-
-//Simplification de calculs :
-
-//$x += 5; //$x = $x + 5;
-//$x -= 5; //$x = $x - 5;
-//$x *= 5; //$x = $x * 5;
-//$x /= 5; //$x = $x / 5;
-//$x **= 5; //$x = $x ** 5;
-//$x %= 5; //$x = $x % 5;
-
-
-$i = 0;
-
-// $i++; // $i = $i + 1;
-
-// $i--; // $i = $i - 1;
-
-// $naissance = 1986;
-
-// $age = date ('Y')- $naissance;
-
-
-// dd($age);
-
-$paht = 34;
-$tva = 10;
-
-$pvttc=$paht+($paht*$tva/100);
-
-dd($pvttc);
+$titre = 'Tableaux PHP';
 
 require './index.vue.php';
