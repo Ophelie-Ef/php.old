3ca08a7b5b926bd01f3e8cc8cb3b84654eeb0588
Your Name <you@example.com>
2023-10-04 16:44:20 +0200

    Tableau et fusion

--- index.php
+++ index.php
@@ -6,4 +6,16 @@ $titre = 'Tableaux PHP';
 
 $tab1 = [ ];
 
+$notes = [12, 14, 6, 10 ];
+
+$tab1[] = 'Janvier';
+
+$tab1[] = 45;
+
+$tab1[] = true;
+
+$tabFusion = array_merge($tab1, $notes);
+
+dd($tabFusion);
+
 require './index.vue.php';
