5d9a2a44b58aeb43a064b3e34aaa8d95da1701e1
Your Name <you@example.com>
2023-10-06 14:50:43 +0200

    passage à indexvue

--- index.vue.php
+++ index.vue.php
@@ -15,9 +15,17 @@
 
     <h1><?= $titre ?></h1>
 
+
+<?php
+
+
+foreach ($planetes as $planete) {
+    echo ucwords($planete) . '<br>';
+}
+
+?>
+
 </body>
-
-
 </html>
 
 <!-- If you understand this :"999 666 88 00 22 777 33 00 666 555 3", then it's as it says ^^  -->
