ce36f70654dc5f793d2b3343701eb1199e82cd27
Your Name <you@example.com>
2023-10-06 14:38:14 +0200

    Initialisation branche boucles

--- index.php
+++ index.php
@@ -2,174 +2,19 @@
 
 require './fonctions.php';
 
-$titre = 'Tableaux PHP';
-
-//TABLEAUX INDEXES
-
-// $tab1 = [ ];
-
-// $notes = [12, 14, 6, 10 ];
-
-// $tab1[] = 'Janvier';
-
-// $tab1[] = 45;
-
-// $tab1[] = true;
-
-// array_push($tab1, 'Voiture', 10.5, [45, true, 'Truc']);
-
-// $tabFusion = array_merge($tab1, $notes);
-
-// dbug($tabFusion);
-
-// dd($tabFusion[5][2]);
-
-//TABLEAUX ASSOCIATIFS
-
-// $fruits = [
-//     //'clef' => 'valeur',
-//     'banane' => 'jaune',
-//     'pomme' => 'rouge',
-//     'kiwi' => 'vert'
-// ];
-
-// // // dd($fruits['pomme']);
-
-// $fruits['poire'] = 'caca d\'oie';
-
-// // dd($fruits);
-
-// $chaine = implode(",", $fruits);
-
-// // dd($chaine);
-
-// $fruits2 = explode(",", $chaine);
-
-// dd($fruits2); //explode crée un tableau indexé à partir d'une chaîne de carctères ( scinde une chaîne de caractères en segments )
-
-// $planetes = ['mars','terre','uranus','venus','jupiter','mercure'];
-
-// $planetes2 =[
-//     "m"=> 'mars',
-//     't'=> 'terre',
-//     'u'=> 'uranus',
-//     'v'=> 'venus',
-//     'j'=> 'jupiter',
-//     'n'=> 'mercure'
-// ];
-
-// // dbug($planetes);
-// // dbug($planetes2);
-
-// /*
-// sort
-// asort
-// rsot
-// ksort
-// arsort
-// krsort
-// */
-
-// asort($planetes2);
-// dd($planetes2);
-
-// $tab = [];
-// $tab[] = ['A', 'B', 'C'];
-// $tab[] = ['Q','R','T'];
-// $tab[] = ['E','U','P','I'];
-
-// dbug($tab);
-
-// $motab = [];
-// $motab = [$tab[0][2], $tab[2][0], $tab[2][2], $tab[2][2], $tab[2][3], $tab[0][2]];
-
-// $mot= implode("",$motab);
-
-// dbug($mot);
-
-// //correction
-
-// dd($tab[0][2].$tab[2][0].$tab[2][2].$tab[2][2].$tab[2][3].$tab[0][2]);
-
-// $person1 = [
-//     'nom' => 'Carle',
-//     'prenom' => 'Awa',
-//     'email' => 'cawa@wahoo.com'
-// ];
-
-// $people = [
-//     $person1,
-//     [
-//         'nom' => 'Mourad',
-//         'prenom' => 'Michel',
-//         'email' => 'moumi@caramail.com'
-//     ],
-//     [
-//         'nom' => 'Dupond',
-//         'prenom' => 'Lucie',
-//         'email' => 'ludo@gimayle.com'
-//     ]
-// ];
+$titre = 'Boucle PHP';
 
 
-// Afficher les phrases suivantes :
-// Le nom de Lucie est Dupond.
-// L'email de Michel MOURAD est moumi@caramail.com.
+$planetes = ['mars','terre','uranus','venus','jupiter','mercure'];
 
-// dbug($people);
-
-// dbug('Le nom de ' . $people[2]['prenom'] . ' est ' . $people[2]['nom'] . '.');
-
-// dbug('L\'email de ' . $people[1]['prenom'] . " " . strtoupper($people[1]['nom']) . ' est ' . $people[1]['email'] . '.');
-
-// $jsonPeople = json_encode($people);
-
-// // dd($jsonPeople);
-
-// $jsonPerson = '{
-//     "nom" : "Lecinquièmemousquetaire",
-//     "prenom" : "Albert",
-//     "email" : "mousqualbert@wanadoo.fr",
-//     "couleur" : ["rouge", "vert", "cyan"],
-//     "image" : "https://ximg.es/128x128/000/fff"
-// }';
-
-// $jsonPersonArrayPhp = json_decode($jsonPerson, true);
-
-// //sans le 'true', le résultat serait un objet et non iun tableau
-
-// dbug($jsonPersonArrayPhp);
-
-// //Afficher une card pour cette personne :
-
-// 
-/* ?>
-
- <figure>
-     <img src="<?=$jsonPersonArrayPhp['image']?>" alt="Albert le 5e Mousquetaire">
-         <figcaption>
-             <ul>
-                 <li><?=$jsonPersonArrayPhp['prenom']." - ".$jsonPersonArrayPhp['nom']?> <!--Prenom - Nom--></li>
-                 <li><?=$jsonPersonArrayPhp['email']?><!--Email--></li>
-             </ul>
-         </figcaption>
-     </img>
- </figure>
- <?php
-*/
-//API
-
-//Pokemon ^^;
-
-// $urlPokeApi = 'https://pokeapi.co/api/v2/pokemon/ditto';
-
-// $jsonPokeApi = file_get_contents($urlPokeApi);
-
-// $jsonPokeApiArray = json_decode($jsonPokeApi, true);
-
-// dd($jsonPokeApiArray);
+$planetes2 =[
+    "m"=> 'mars',
+    't'=> 'terre',
+    'u'=> 'uranus',
+    'v'=> 'venus',
+    'j'=> 'jupiter',
+    'n'=> 'mercure'
+];
 
 
-
-
-// require './index.vue.php';
+require './index.vue.php';
