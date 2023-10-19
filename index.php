08a0320301813ad23d82b07bc5da7107b0fc789a
Ophelie <freville.ophelie@neuf.fr>
2023-10-19 15:22:44 +0200

    Préparation

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 require './fonctions.php';
 
-//dd($_SERVER);
-// dd($_SERVER['REQUEST_URI']);
-
-// dd(parse_url($_SERVER['REQUEST_URI'])['path']);
-
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-// dd($uri);
-
-if ($uri === '/php/'):
-    echo 'Je suis à la racine de mon dossier de travail.';
-elseif ($uri ==='php/contact/'):
-    echo 'contact.php';
-endif;
+$routes = [
+    '/php' => 'controllers/index.php',
+    '/php/contact' => 'controllers/contact.php',
+    '/php/notes' => 'controllers/notes.php'
+];
