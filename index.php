74395a9213d55d0ff5f3556c4374456c9d84158d
Ophelie <freville.ophelie@neuf.fr>
2023-10-24 12:14:17 +0200

    Router, déplacements

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
-require './fonctions.php';
-
-
-$uriPath = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/php' => 'controllers/index.php',
-    '/php/contact' => 'controllers/contact.php',
-    '/php/notes' => 'controllers/notes.php'
-];
-
-$urlToController = match($uriPath){
-    '/' =>  'controllers/index.php',
-    '/contact' => 'controllers/contact.php'
-};
-
-require $urlToController;
-
-exit();
+require 'config/fonctions.php';
+require 'config/router.php';
--- views/index.view.php
+++ views/index.view.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
     <title>Index</title>
 </head>
+
 <body>
     <h1>Index</h1>
 </body>
+
 </html>
